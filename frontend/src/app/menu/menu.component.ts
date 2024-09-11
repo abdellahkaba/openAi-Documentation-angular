23e6af7bc35b3078f31b1dccb813d8cab4cdf854
Abdellah <abdallahkaba98@gmail.com>
2024-09-11 20:22:53 +0000

    main-and-menu-component

---
+++ frontend/src/app/menu/menu.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import {RouterLink, RouterOutlet} from "@angular/router";
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    RouterLink
+  ],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+export class MenuComponent {
+
+}
