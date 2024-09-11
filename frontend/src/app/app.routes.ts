b6128d0c1a8da27198c4681551ef8903b6fda6c7
Abdellah <abdallahkaba98@gmail.com>
2024-09-11 19:18:21 +0000

    init-configuration-angular

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  {
-    path: 'categories',
-    loadChildren: () => import('./modules/category/category.module').then(m => m.CategoryModule)
-  }
-];
+export const routes: Routes = [];
